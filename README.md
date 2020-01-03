aac8724cb1c67845c7f154d632e80b97523cea30
Qiu Siyuan <truxihere@gmail.com>
2020-01-03 20:19:04 +0000

    call rest

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # springboot-play
 playground spring boot project
+
+# Run
+1. Build & Run:
+```
+./gradlew build && java -jar build/libs/gs-rest-service-0.1.0.jar
+```
+2. Run
+```
+./gradlew bootRun
+```
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GreetingController {
@@ -15,5 +16,16 @@ public class GreetingController {
   public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
     return new Greeting(counter.incrementAndGet(),
               String.format(template, name));
+  }
+
+  @RequestMapping("/messege")
+  public Greeting messege() {
+    final String uri = "https://gturnquist-quoters.cfapps.io/api/random";
+     
+    RestTemplate restTemplate = new RestTemplate();
+    Quote result = restTemplate.getForObject(uri, Quote.class);
+
+    return new Greeting(counter.incrementAndGet(),
+              result.getValue().getQuote());
   }
 }

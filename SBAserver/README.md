c33d74aa174ef1c5771646a521018f361c28a40f
Qiu Siyuan <truxihere@gmail.com>
2020-01-07 19:28:23 +0000

    sba

---
+++ SBAserver/README.md
@@ -0,0 +1,4 @@
+# What's in
+Spring Boot Admin 2.2.0 server
+
+deployed using port 8089
--- SBAserver/src/main/java/com/qiusiyuan/SBAserver/SbAserverApplication.java
+++ SBAserver/src/main/java/com/qiusiyuan/SBAserver/SbAserverApplication.java
@@ -2,8 +2,10 @@ package com.qiusiyuan.SBAserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SbAserverApplication {
 
 	public static void main(String[] args) {
